7294fde45eb3ce0b58f79aabaafe34fc9a090839
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-12-15 12:45:13 +0200

    Women TechPower in master blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content=" …">
-    <title>Exemplu creare Blade</title>
+    <title>Women TechPower</title>
     @include('partials.styles')
 </head>
 <body>
